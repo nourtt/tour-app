3a7c80580a79bb2a09c856d557ed4413c0d038ed
Nurassyl Mukan <nurasylmukan@gmail.com>
2025-06-10 23:45:58 +0500

    файлы: создал компонент TourCard

---
+++ src/features/tour-card/ui/TourCard.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Card, Button, Typography } from "antd";
+import { useAppDispatch } from "../../../app/store/hooks";
+import type {Tour} from "../../../entities/tour/model/types"
+import * as bookingActions from "../../tour-booking/model/bookingSlice"
+
+const {Title, Text} = Typography;
+
+export const TourCard: React.FC<{tour: Tour}> = ({tour}) => {
+    const dispatch = useAppDispatch();
+    //Функция для активации модального окна при нажатии
+    const handleBookClick = () => {
+        dispatch(bookingActions.openModal(tour.id))
+    }
+    return (
+        <Card
+            cover={<img alt={tour.title} src={tour.image}></img>}
+            style={{width: 300}}
+        >
+            <Title level={4}>{tour.title}</Title>
+            <Text>{tour.country}</Text>
+            <br />
+            <Text>{tour.price}</Text>
+            <br />
+            <Text>{tour.startDate}</Text>
+            <br />
+            <Button
+                type="primary"
+                onClick={handleBookClick}
+                style={{marginTop: 16}}
+            >
+                Забронировать
+            </Button>
+        </Card>
+    )
+}
