3e159f955d2e8ebaae43f0687b275ec9473dfb0f
Nurassyl Mukan <nurasylmukan@gmail.com>
2025-06-10 20:05:44 +0500

    файлы: добавил функциональность в bookingSlice.ts, написал редьюсеры

---
+++ src/features/tour-booking/model/bookingSlice.ts
@@ -0,0 +1,79 @@
+import { createSlice } from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
+//Инициализация стейтов
+interface BookingFormState {
+    name: string;
+    phone: string;
+    email: string;
+}
+
+interface BookingState {
+    selectedTourID: string | null;
+    isModalOpen: boolean;
+    form: BookingFormState;
+}
+
+const initialState: BookingState = {
+    selectedTourID: null,
+    isModalOpen: false,
+    form: {
+        name: '',
+        phone: '',
+        email: '',
+    },
+};
+//Слайс
+const bookingSlice = createSlice({
+    name: 'booking',
+    initialState,
+    reducers: {
+        /**
+         * 
+         * @param редьюсер для открытия модального окна брони
+         * @param использует PayloadAction для сохранности типа
+         */
+        openModal: (state, action: PayloadAction<string>) => {
+            state.selectedTourID = action.payload;
+            state.isModalOpen = true;
+        },
+        /**
+         * 
+         * @param редьюсер для закрытия модального окна брони
+         */
+        closeModal: (state) => {
+            state.isModalOpen = false;
+            state.selectedTourID = null;
+        },
+        /**
+         * 
+         * @param редьюсер для обноваления формы
+         * @param использует PayloadAction для сохранности типа
+         */
+        updateFormField: (
+            state,
+            action: PayloadAction<{field: keyof BookingFormState; value: string}>
+        ) =>{
+            const {field, value} = action.payload;
+            state.form[field] = value;
+        },
+        /**
+         * 
+         * @param редьюсер для сброса формы
+         */
+        resetForm: (state) => {
+            state.form = initialState.form;
+        },
+        /**
+         * 
+         * @param редьюсер для публикации формы
+         */
+        submitForm: (state) => {
+            state.isModalOpen = false;
+            state.selectedTourID = null;
+            state.form = initialState.form;
+        },
+    },
+});
+
+export const {openModal, closeModal, updateFormField, resetForm, submitForm } = bookingSlice.actions;
+export default bookingSlice.reducer;
