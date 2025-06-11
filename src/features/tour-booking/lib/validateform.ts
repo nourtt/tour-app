72ae54bd63d2a696c365c442940c7050e62e84dd
Nurassyl Mukan <nurasylmukan@gmail.com>
2025-06-11 11:20:54 +0500

    изменено: улучшил валидацию имейла

--- src/features/tour-booking/lib/validateform.ts
+++ src/features/tour-booking/lib/validateform.ts
@@ -1,12 +1,12 @@
 import {isValidPhoneNumber} from 'libphonenumber-js';
-
+import validator from 'validator';
 
 export const validateForm = (form: {name: string; phone: string; email: string}): string | null => {
     if (!form.name.trim()) return 'Укажите имя';
     if (!form.phone.trim()) return 'Укажите телефон';
     if (!isValidPhoneNumber(form.phone, 'KZ')) return 'Некорректный номер телефона'
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //для проверки валидности введенного имейла
-    if (!emailRegex.test(form.email)) return 'Некорректный email';
+
+    if (!validator.isEmail(form.email)) return 'Некорректный email';
 
     return null;
 }
